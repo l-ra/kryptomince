1521b276e29ea5e866c4aae32611530cb5264bff
lra <lra@r73.info>
2023-05-09 11:12:20 +0200

    added cache

--- block-by-number.php
+++ block-by-number.php
@@ -4,11 +4,20 @@ include "apikey.php";
 $blockNo = $_REQUEST["block_no"];
 
 if ($blockNo && preg_match("/^0x[0-9a-fA-F]+$/",$blockNo)) {
-    header('Content-Type: application/json');
-	readfile(sprintf("https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=%s&boolean=true&apikey=%s",$blockNo,$apikey));
+	$blockCacheFile = $cacheDir . "block-".$blockNo;
+	header('Content-Type: application/json');
+	if ( file_exists($blockCacheFile)){
+		$response = file_get_contents($blockCacheFile);
+	} else {
+		$response = file_get_contents(sprintf("https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=%s&boolean=true&apikey=%s",$blockNo,$apikey));
+		if (preg_match("/".$blockNo."/",$response)){
+			# have result - store it
+			file_put_contents($blockCacheFile,$response);
+		}
+	}
+	print($response);
 } else {
 	print('{"result":null}');
 }
-
 ?>
 
--- block-number.php
+++ block-number.php
@@ -1,7 +1,20 @@
 <?php
 include "apikey.php";
-
+$cacheFile = $cacheDir . "last-block-no";
 header('Content-Type: application/json');
-readfile(sprintf("https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=%s",$apikey));
+
+if (file_exists($cacheFile)) {
+	$cacheMtime = filemtime($cacheFile);
+} else {
+	$cacheMtime = false;
+}
+if ($cacheMtime && $cacheMtime>time()-2){
+ 	$response = file_get_contents($cacheFile);
+} else {
+	
+	$response = file_get_contents(sprintf("https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=%s",$apikey));
+	file_put_contents($cacheFile,$response);
+}
+print($response)
 
 ?>
